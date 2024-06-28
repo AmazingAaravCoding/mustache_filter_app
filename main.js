82d49d4605984f7647051d6314e40bfaaa5da16e
AmazingAaravCoding <142331565+AmazingAaravCoding@users.noreply.github.com>
2024-06-28 17:37:37 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 function preload() {
     mustache = loadImage("mustache.png")
+    nx = 0
+    ny = 0
 }
 function setup() {
-    canvas = createCanvas(400, 300)
+    canvas = createCanvas(750, 600)
     canvas.center()
     video = createCapture(VIDEO)
     video.hide()
-    video.size(400, 300)
+    video.size(750, 600)
     posenet = ml5.poseNet(video, modelloaded)
     posenet.on("pose", gotposes)
 }
 function draw() {
-    image(video, 0, 0, 400, 300)
-    image(mustache, 200, 200, 75, 50)
+    image(video, 0, 0, 750, 600)
+    image(mustache, nx, ny, 75, 50)
 }
 function takeSnapshot() {
-
+    save("mustache-selfie.png")
 }
 function modelloaded() {
     console.log("model is ready");
 }
 function gotposes(result) {
-    if (condition) {
+    if (result.length > 0) {
         console.log(result);
-        nx = result[0].pose.nose.x
+        nx = result[0].pose.nose.x - 37
         ny = result[0].pose.nose.y
     }
 }
